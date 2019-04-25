f448dfab4fce1df3b25544fd1cceaf7734408bf9
glewkk <glewkk@gmail.com>
2019-04-25 13:06:29 +0300

    Add: Working project

---
+++ Working Copy/testareSoftware/testareSoftware/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(testareSoftware.Startup))]
+namespace testareSoftware
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
